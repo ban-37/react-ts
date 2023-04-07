71b6625ade25a0e1fc691427780694cc10676644
椰子 <mengmuzi3721@163.com>
2023-04-07 17:57:45 +0800

    Delete my_folder from the repository

--- src/api/auth.ts
+++
@@ -1,6 +0,0 @@
-import {UserType} from "../ type"
-import  request from "../utils/request"
-
-export const Lgoin = ( data:UserType) => {
-return request.post("/api/login" , data)
-}
--- src/router/index.jsx
+++
@@ -1,9 +0,0 @@
-import baseRouter from "./baseRouter";
-import { useRoutes } from "react-router-dom";
-
-
-function RouterView ()  {
-    const element = useRoutes(baseRouter)
-    return ( <>{element}</>)
-}
-export default RouterView
